f7f599b9a3a82fa818f299e432f7e9d891ccb9e1
mpcooper23 <mpcooper23@gmail.com>
2024-12-18 11:35:02 -0600

    1

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -21,27 +21,124 @@ var _ = require('underbar');
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
+/**
+ * ### 1: `maleCount`
+ - **Objective**: Find the number of male customers
+ - **Input**: `Array`
+ - **Output**: `Number`
+ - **Constraints**: use `filter`
+ * 
+ */
+
 var maleCount = function(array) {
 
 };
 
+/** 
+2: femaleCount
+Objective: Find the number of female customers
+Input: Array
+Output: Number
+Constraints: use reduce
+ * 
+*/
+
 var femaleCount;
+
+/** 
+ * 3: oldestCustomer
+Objective: Find the oldest customer's name
+Input: Array
+Output: String
+Constraints:
+ * 
+*/
 
 var oldestCustomer;
 
+/** 
+ * 4: youngestCustomer
+Objective: Find the youngest customer's name
+Input: Array
+Output: String
+Constraints:
+ * 
+*/
+
 var youngestCustomer;
+
+/** 
+ * 5: averageBalance
+Objective: Find the average balance of all customers
+Input: Array
+Output: Number
+Constraints:
+ * 
+*/
 
 var averageBalance;
 
+/** 
+ * 6: firstLetterCount
+Objective: Find how many customer's names begin with a given letter
+Input: Array, Letter
+Output: Number
+Constraints:
+ * 
+*/
+
 var firstLetterCount;
+
+/** 
+ * 7: friendFirstLetterCount
+Objective: Find how many friends of a given customer have names that start with a given letter
+Input: Array, Customer, Letter
+Output: Number
+Constraints:
+ * 
+*/
 
 var friendFirstLetterCount;
 
+/** 
+8: friendsCount
+Objective: Find the customers' names that have a given customer's name in their friends list
+Input: Array, Name
+Output: Array
+Constraints:
+ * 
+*/
+
 var friendsCount;
+
+/** 
+ * 9: topThreeTags
+Objective: Find the three most common tags among all customers' associated tags
+Input: Array
+Output: Array
+Constraints:
+ * 
+*/
 
 var topThreeTags;
 
+/** 
+ * 10: genderCount
+Objective: Create a summary of genders, the output should be:
+{
+    male: 3,
+    female: 4,
+    non-binary: 1
+}
+Input: Array
+Output: Object
+Constraints: Use reduce
+ * 
+*/
+
 var genderCount;
+
+
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
